fc90a12985d30bc7b508c44a500d28742af21869
carrie <carrie0129@naver.com>
2021-02-23 21:56:28 +0900

    add project setting

--- app/src/main/java/com/example/lifoo/src/SplashActivity/SplashActivity.java
+++ app/src/main/java/com/example/lifoo/src/SplashActivity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.example.lifoo_add;
+package com.example.lifoo.src.SplashActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lifoo.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
