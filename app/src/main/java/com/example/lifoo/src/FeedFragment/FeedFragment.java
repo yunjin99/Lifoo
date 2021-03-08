59d14c576c819a99e9add96be9b713e304f460cb
JiYoungDo <carrie990129@gmail.com>
2021-03-08 10:36:21 +0900

    add four fragment and setitngs

---
+++ app/src/main/java/com/example/lifoo/src/FeedFragment/FeedFragment.java
@@ -0,0 +1,41 @@
+package com.example.lifoo.src.FeedFragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.lifoo.R;
+import com.example.lifoo.src.MainActivity.MainActivity;
+
+public class FeedFragment  extends Fragment {
+
+    MainActivity mainActivity;
+    ViewGroup viewGroup;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) getActivity();
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mainActivity = null;
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_feed,container,false);
+        return viewGroup;
+    }
+}
